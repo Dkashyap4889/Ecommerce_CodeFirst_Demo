9ad5a7aabc4da448fa05a67bf09c0dfe3f8c1bb2
Dheeraj Kashyap <dkashyap@covalience.int>
2026-02-06 15:15:33 +0530

    Completed Assignment

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Ecommerce_CodeFirst_Demo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce_CodeFirst_Demo.Context
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+        }
+    }
+}
